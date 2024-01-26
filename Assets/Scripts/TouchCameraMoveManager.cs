e5d71eee514ba4163f14b5b7249ef8b7ba83c44d
Sincer1ty <87846251+Sincer1ty@users.noreply.github.com>
2024-01-27 02:38:32 +0900

    Add TochCameraMoveManager.cs
    카메라 조작 : 스왑

---
+++ Assets/Scripts/TouchCameraMoveManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchCameraMoveManager : MonoBehaviour
+{
+    private float speed = 0.25f;
+    private Vector2 nowPos, prePos;
+    private Vector3 movePos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount == 1)
+        {
+
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                prePos = touch.position - touch.deltaPosition;
+            }
+            else if (touch.phase == TouchPhase.Moved)
+            {
+                nowPos = touch.position - touch.deltaPosition;
+                movePos = (Vector3)(prePos - nowPos) * Time.deltaTime * speed;
+                transform.Translate(movePos);
+                prePos = touch.position - touch.deltaPosition;
+            }
+        }
+    }
+}
