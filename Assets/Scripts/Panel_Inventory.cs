4478c4c4a338faf987043cb4e9ff5eef9ae8f123
Sincer1ty <87846251+Sincer1ty@users.noreply.github.com>
2024-02-13 00:13:02 +0900

    Add Status Bar
    - 마을 레벨 바 UI 생성
    - 판다 선택 시 이미지 및 설명 변경
    - 한자 지원 폰트로 변경

--- Assets/Scripts/Panel_Inventory.cs
+++ Assets/Scripts/Panel_Inventory.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Text;
 using TMPro;
+using UnityEngine.UI;
 
 [System.Serializable]
 public class CharInfo
 {
     public string charName;
+    [TextArea]
+    public string explanation;
+    public Sprite charSprite;
 }
 
 public class Panel_Inventory : MonoBehaviour
@@ -17,7 +21,9 @@ public class Panel_Inventory : MonoBehaviour
     public List<CharInfo> CharInfoList = new List<CharInfo>();
     List<GameObject> MyCharList = new List<GameObject>();
 
+    public TMP_Text ExNameTxt;
     public TMP_Text ExplanationTxt;
+    Image img;
 
     // Start is called before the first frame update
     void Awake()
@@ -41,6 +47,8 @@ public class Panel_Inventory : MonoBehaviour
 
     private void Start()
     {
+        img = MyChilderen["Image_big"].GetComponent<Image>();
+
         CreateCharList(CharInfoList);
     }
 
@@ -72,6 +80,7 @@ public class Panel_Inventory : MonoBehaviour
                 //슬롯 위치 및 크기 설정
                 NewChar.transform.SetParent(CharSlot.transform.parent);
                 NewChar.transform.localScale = CharSlot.transform.localScale;
+                NewChar.transform.Find("Image").GetComponent<Image>().sprite = CharInfoList[i].charSprite;
 
                 NewChar.SetActive(true);
                 MyCharList.Add(NewChar);
@@ -82,15 +91,34 @@ public class Panel_Inventory : MonoBehaviour
     public void Show()
     {
         transform.gameObject.SetActive(true);
+
+        //패널 배경 일시정지
+        Time.timeScale = 0;
+    }
+
+    public void Close()
+    {
+        transform.gameObject.SetActive(false);
+
+        //패널 배경 재시동
+        Time.timeScale = 1f;
     }
 
     public void OnClicked(GameObject clickedObj)
     {
+        //Console print
         StringBuilder builder = new StringBuilder();
         builder.Append("OnClicked : ");
         builder.Append(clickedObj.name);
         print(builder.ToString());
 
-        //ExplanationTxt.text = CharInfoList[idx].charName;
+        bool isEquals(GameObject listObj)
+        {
+            return listObj.Equals(clickedObj);
+        }
+        int idx = MyCharList.FindIndex(isEquals);
+        ExNameTxt.text = CharInfoList[idx].charName;
+        ExplanationTxt.text = CharInfoList[idx].explanation;
+        img.sprite = clickedObj.transform.Find("Image").GetComponent<Image>().sprite;
     }
 }
