19994377482d23b0f16e94ce2dfba473b41fa8d6
Sincer1ty <87846251+Sincer1ty@users.noreply.github.com>
2024-04-29 02:37:53 +0900

    Add PersistentObject
    - PersistentObject 추가

---
+++ Assets/Scripts/PersistentObjectSpawner.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentObjectSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject persistentObjectPrefab;
+
+    static bool hasSpawned = false;
+
+    private void Awake()
+    {
+        if (hasSpawned) return;
+
+        SpawnPersistentObjects();
+
+        hasSpawned = true;
+    }
+
+    private void SpawnPersistentObjects()
+    {
+        GameObject persistentObject = Instantiate(persistentObjectPrefab);
+        DontDestroyOnLoad(persistentObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
