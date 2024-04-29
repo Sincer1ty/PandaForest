4386585cb422f098484dc32c6474c62078bb4dc7
Sincer1ty <87846251+Sincer1ty@users.noreply.github.com>
2024-04-30 01:35:21 +0900

    Add Audio
    - Audio 추가

--- Assets/Scripts/PersistentObjectSpawner.cs
+++ Assets/Scripts/PersistentObjectSpawner.cs
@@ -24,15 +24,4 @@ public class PersistentObjectSpawner : MonoBehaviour
         DontDestroyOnLoad(persistentObject);
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Scripts/Saving/JsonSaveableEntity.cs
+++ Assets/Scripts/Saving/JsonSaveableEntity.cs
@@ -3,11 +3,9 @@ using Newtonsoft.Json.Linq;
 using UnityEditor;
 using UnityEngine;
 
-//여기부터 확인
 [ExecuteAlways]
 public class JsonSaveableEntity : MonoBehaviour
-{
-    //강의 참조 필요
+{    
     [SerializeField] string uniqueIdentifier = "";
 
     static Dictionary<string, JsonSaveableEntity> globalLookup = new Dictionary<string, JsonSaveableEntity>();
@@ -17,18 +15,73 @@ public class JsonSaveableEntity : MonoBehaviour
         return uniqueIdentifier;
     }
 
-    //Blog 설명
     public JToken CaptureAsJToken()
     {
         JObject state = new JObject();
         IDictionary<string, JToken> stateDict = state;
         foreach (IJsonSaveable jsonSaveable in GetComponents<IJsonSaveable>())
         {
+            //?
             JToken token = jsonSaveable.CaptureAsJToken();
             string component = jsonSaveable.GetType().ToString();
             Debug.Log($"{name} Capture {component} = {token.ToString()}");
             stateDict[jsonSaveable.GetType().ToString()] = token;
         }
         return state;
+    }
+
+    public void RestoreFromJToken(JToken s)
+    {
+        JObject state = s.ToObject<JObject>();
+        IDictionary<string, JToken> stateDict = state;
+        foreach(IJsonSaveable jsonSaveable in GetComponents<IJsonSaveable>())
+        {
+            string component = jsonSaveable.GetType().ToString();
+            if(stateDict.ContainsKey(component))
+            {
+                Debug.Log($"{name} Restore {component} => {stateDict[component].ToString()}");
+                jsonSaveable.RestroreFromJToken(stateDict[component]);
+            }
+        }
+    }
+
+#if UNITY_EDITOR
+    private void Update()
+    {
+        if (Application.IsPlaying(gameObject)) return;
+        if (string.IsNullOrEmpty(gameObject.scene.path)) return;
+
+        SerializedObject serializedObject = new SerializedObject(this);
+        SerializedProperty property = serializedObject.FindProperty("uniqueIdetifier");
+
+        if (string.IsNullOrEmpty(property.stringValue) || !IsUnique(property.stringValue))
+        {
+            property.stringValue = System.Guid.NewGuid().ToString();
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        globalLookup[property.stringValue] = this;
+    }
+#endif
+
+    private bool IsUnique(string candidate)
+    {
+        if (!globalLookup.ContainsKey(candidate)) return true;
+
+        if (globalLookup[candidate] == this) return true;
+
+        if(globalLookup[candidate] == null)
+        {
+            globalLookup.Remove(candidate);
+            return true;
+        }
+
+        if(globalLookup[candidate].GetUniqueIdentifier() != candidate)
+        {
+            globalLookup.Remove(candidate);
+            return true;
+        }
+
+        return false;
     }
 }
